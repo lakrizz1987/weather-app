001b7892f2f212e9f6d898304c7c91c83f0ff7f8
Ivaylo <ignatov.ivaylo@gmail.com>
2023-01-23 14:32:48 +0200

    styling Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
     return (
-        <header className="site-header">
+        <header className={styles['site-header']}>
             <h1>WeatherApp</h1>
             <section className="site-search-container">
                 <form method="post">
-                    <input type="text" id="search" />
-                    <input type="submit" name="submit" value='&#128269;' />
+                    <input className={styles.searchInput} type="text" id="search" placeholder="Search a town..." />
+                    <input className={styles.searchBtn} type="submit" name="submit" value='&#128269;' />
                 </form>
             </section>
         </header>
