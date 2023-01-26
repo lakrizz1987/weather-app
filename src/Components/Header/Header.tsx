8cc23f629c034054bb70b7581ce1e6e67c3beb7a
Ivaylo <ignatov.ivaylo@gmail.com>
2023-01-26 13:56:55 +0200

    add submitHadler to search input and read it

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,12 +3,20 @@ import styles from "./Header.module.css";
 
 
 const Header: React.FC = () => {
+
+    function submitHadler(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        
+        let { town } = Object.fromEntries(new FormData(event.currentTarget));
+        event.currentTarget.reset();
+    }
+
     return (
         <header className={styles['site-header']}>
             <h1>WeatherApp</h1>
             <section className="site-search-container">
-                <form method="post">
-                    <input className={styles.searchInput} type="text" id="search" placeholder="Search a town..." />
+                <form onSubmit={submitHadler} method="post">
+                    <input className={styles.searchInput} type="text" id="search" placeholder="Search a town..." name="town" />
                     <input className={styles.searchBtn} type="submit" name="submit" value='&#128269;' />
                 </form>
             </section>
--- src/services/services.tsx
+++ src/services/services.tsx
@@ -10,13 +10,11 @@ const apiKey = 'd5a876fbd3439597f908bd3b1cd6fbec';
 const getCurrentWeatherByLat = async (lat:number,lon:number) => {
 
     try {
-
-        
         const respons = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat.toString()}&lon=${lon.toString()}&appid=${apiKey}&units=metric`);
 
         if (!respons.ok) {
-            const result = await respons.json();
-            throw new Error(result.message)
+            const resultErr = await respons.json();
+            throw new Error(resultErr.message)
         }
 
         const result = await respons.json();
