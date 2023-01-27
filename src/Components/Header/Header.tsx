1cb3ee387711dcca8f860c0a5b0b7717933a3a9c
Ivaylo <ignatov.ivaylo@gmail.com>
2023-01-27 13:15:25 +0200

    add err notification when input is empty

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,41 +1,44 @@
 import React, { useState } from "react";
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import styles from "./Header.module.css";
 import api from "../../services/services";
-import {townConstuctor} from '../../helpers';
+import { townConstuctor } from '../../helpers';
 import { setTown } from "../../store/townSlice";
 
 const Header: React.FC = () => {
     const dispatch = useDispatch();
-    const [isEmptySearchInput,setIsEmptySearchInput] = useState(false);
+    const [isEmptySearchInput, setIsEmptySearchInput] = useState(false);
 
     async function submitHadler(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        
+
         let { town } = Object.fromEntries(new FormData(event.currentTarget));
-        if(town === ''){
+        if (town === '') {
             setIsEmptySearchInput(true)
-        }else{
+        } else {
             event.currentTarget.reset();
 
             const searchedTown = await api.getCurrentWeatherByName(town.toString());
-    
+
             dispatch(setTown(townConstuctor(searchedTown)));
         }
     }
 
-    let styleWarning=styles.errMsg;
-    if(isEmptySearchInput){
-        styleWarning = styles.errMsg + ' ' + styles.errMsgShow;
+    let styleWarningSpan = styles.errMsg;
+    let styleWarningInput = styles.searchInput;
+    if (isEmptySearchInput) {
+        styleWarningSpan = styles.errMsg + ' ' + styles.errMsgShow;
+        styleWarningInput = styles.searchInput + ' ' + styles.errInput;
     }
+
 
     return (
         <header className={styles['site-header']}>
             <h1>WeatherApp</h1>
             <section className="site-search-container">
-                    <p onClick={()=>setIsEmptySearchInput(false)} className={styleWarning}>Field can't be empty</p>
                 <form onSubmit={submitHadler} method="post">
-                    <input onFocus={()=>setIsEmptySearchInput(false)} className={styles.searchInput} type="text" id="search" placeholder="Search a town..." name="town" />
+                <span className={styleWarningSpan}>&#10007;</span>
+                    <input onFocus={() => setIsEmptySearchInput(false)} className={styleWarningInput} type="text" id="search" placeholder="Search a town..." name="town" />
                     <input className={styles.searchBtn} type="submit" name="submit" value='&#128269;' />
                 </form>
             </section>
